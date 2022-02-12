dc15d7674da59f37fe75f85547dc3df6854b0e51
gemmaro <gemmaro.dev@gmail.com>
2022-02-12 22:03:28 +0900

    [ translate ] Generate Gettext POT file for Japanese translation

---
+++ src/Tutorial/generate_pot.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+po4a_config_file_path=po4a.cfg
+
+echo '[po_directory] po/' > $po4a_config_file_path
+
+for markdown_file_path in *.md; do
+	markdown_basename=$(basename "$markdown_file_path")
+	echo "[type: text] $markdown_basename \$lang:po/\$lang/$markdown_basename opt:\"--option markdown --option neverwrap\"" >> $po4a_config_file_path
+done
