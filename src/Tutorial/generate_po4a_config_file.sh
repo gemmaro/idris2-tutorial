6b57e5bb9e4d3c88d222adcb5d2bf42be43ab8e1
gemmaro <gemmaro.dev@gmail.com>
2022-02-19 11:11:43 +0900

    [ translate ] Translate Intro chapter into Japanese
    * [ translate ] Translate untill maxBits8 example
    
    * [ translate ] Improve translation fluency, and translate Total
                    Functions section
    
    * [ translate ] Translate untill Total Functions section
    
    * [ fix ] po4a config generator filename

--- src/Tutorial/generate_po4a_config_file.sh
+++ src/Tutorial/generate_po4a_config_file.sh
@@ -6,5 +6,5 @@ echo '[po_directory] po/' > $po4a_config_file_path
 
 for markdown_file_path in *.md; do
 	markdown_basename=$(basename "$markdown_file_path")
-	echo "[type: text] $markdown_basename \$lang:po/\$lang/$markdown_basename opt:\"--option markdown --option neverwrap\"" >> $po4a_config_file_path
+	echo "[type: text] $markdown_basename \$lang:po/\$lang/$markdown_basename opt:\"--option markdown --option neverwrap\" --keep 0" >> $po4a_config_file_path
 done
